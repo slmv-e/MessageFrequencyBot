c4a95f9832589bf903c0c154d91190b4a7b5c846
slmv-e <fiddler-mush0i@icloud.com>
2023-02-16 05:38:22 +0300

    Bug fixes

--- sheets_worker.py
+++ sheets_worker.py
@@ -8,25 +8,24 @@ from data import UsersInfo
 class Sheets:
     def __init__(self, users_info: UsersInfo = None):
         self.users_info = users_info
-        self.sh = gspread.service_account(filename="spatial-flag-367222-d4d647987359.json").open("AverageTime")
+        self.wkh = gspread.service_account(filename="spatial-flag-367222-d4d647987359.json").open("AverageTime").worksheet("Sheet")
 
     def worker(self):
-        wkh = self.sh.worksheet("Sheet")
         time_now = dt.datetime.now(tz=dt.timezone(dt.timedelta(hours=3)))
-        wkh.add_cols(cols=1)
-        wkh.update(
-            f"R1C{wkh.col_count}",
+        self.wkh.update(
+            f"R1C{self.wkh.col_count}",
             f"{'0' + str(time_now.day) if len(str(time_now.date())) == 1 else time_now.day}.{'0' + str(time_now.month) if len(str(time_now.month)) == 1 else time_now.month}"
         )
         for user in self.users_info.users:
             if user.messages_difference:
                 average_time = int(sum(user.messages_difference) / len(user.messages_difference))
-                if user_cell := wkh.find(user.full_name):
-                    wkh.update(f"R{user_cell.row}C{wkh.col_count}", average_time)
+                if user_cell := self.wkh.find(user.full_name):
+                    self.wkh.update(f"R{user_cell.row}C{self.wkh.col_count}", average_time)
                 else:
-                    wkh.add_rows(rows=1)
-                    wkh.update(f"R{wkh.row_count}C1", user.full_name)
-                    wkh.update(f"R{wkh.row_count}C{wkh.col_count}", average_time)
+                    self.wkh.update(f"R{self.wkh.row_count}C1", user.full_name)
+                    self.wkh.update(f"R{self.wkh.row_count}C{self.wkh.col_count}", average_time)
+                    self.wkh.add_rows(rows=1)
+        self.wkh.add_cols(cols=1)
         self.users_info.users = []
 
     def start(self):
